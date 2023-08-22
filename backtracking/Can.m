347edf04efc5348f1dc52977c21ee5734a0c5909
kldoudou <136078526+kldoudou@users.noreply.github.com>
2023-08-22 17:56:38 +0800

    Delete backtracking directory

--- backtracking/Can.m
+++
@@ -1,6 +0,0 @@
-function bool = Can(G,v,colour)
-%check whether it can use not contradiction with its neighbourhood
-    N = neighbors(G,v);
-    judge = colour(N) ~= colour(v);
-    bool = all(judge,2);
-end
